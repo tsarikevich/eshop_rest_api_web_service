353747215db1da857cfc1e61dd3f3dafa27e7af1
tsarikevich <ma.tsarikevich@gmail.com>
2022-07-23 18:37:00 +0300

    Initial commit

---
+++ src/main/java/by/teachmeskills/eshop/EshopRestApiWebserviceApplication.java
@@ -0,0 +1,13 @@
+package by.teachmeskills.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopRestApiWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopRestApiWebserviceApplication.class, args);
+    }
+
+}
